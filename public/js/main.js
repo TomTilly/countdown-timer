10bb64d71e6d393decf93dab2dd328179e9e4b8d
Tom Tilly <tomtillistrand@gmail.com>
2019-07-10 23:25:35 -0400

    remove unnecessary global vars

--- public/js/main.js
+++ public/js/main.js
@@ -3,50 +3,38 @@ const customMinForm = document.querySelector('.custom-min-form');
 const customMinInput = customMinForm.querySelector('.custom-min-form__input');
 const endTimeEl = document.querySelector('.timer__end-time');
 const timerDisplay = document.querySelector('.timer__countdown');
-let now;
-let then;
-let totalTimeInSeconds;
 let intervalID;
 
-function getMinutes(timeInSeconds) {
-  return Math.floor(timeInSeconds / 60);
-}
-
-function getSeconds(timeInSeconds) {
-  return timeInSeconds % 60;
-}
-
-function updateTimerDisplay() {
-  const seconds = getSeconds(totalTimeInSeconds);
-  const minutes = getMinutes(totalTimeInSeconds);
+function updateTimerDisplay(secondsLeft) {
+  const seconds = secondsLeft % 60;
+  const minutes = Math.floor(secondsLeft / 60);
   const display = `${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
   timerDisplay.textContent = display;
   document.title = display;
 }
 
-function displayEndTime() {
-  const end = new Date(then);
+function displayEndTime(endTime) {
+  const end = new Date(endTime);
   const hour = end.getHours();
   const minutes = end.getMinutes();
   endTimeEl.textContent = `${hour}:${minutes < 10 ? '0' : ''}${minutes}`;
 }
 
-function decrementTimer() {
-  totalTimeInSeconds = Math.round((then - Date.now()) / 1000);
-  if (totalTimeInSeconds === 0) {
+function decrementTimer(secondsLeft) {
+  if (secondsLeft === 0) {
     clearInterval(intervalID);
   }
-  updateTimerDisplay();
+  updateTimerDisplay(secondsLeft);
 }
 
 function startTimer(e) {
   e.preventDefault();
 
   // dataset.time will be undefined if form is submitted, in which case get value from form input
-  totalTimeInSeconds = parseInt((e.target.dataset.time || customMinInput.value), 10) * 60;
+  const totalSeconds = parseInt((e.target.dataset.time || customMinInput.value), 10) * 60;
 
   // Only handle input < 1 hour
-  if (totalTimeInSeconds > 3600) {
+  if (totalSeconds > 3600) {
     return;
   }
 
@@ -55,11 +43,14 @@ function startTimer(e) {
     clearInterval(intervalID);
   }
 
-  now = Date.now();
-  then = now + (totalTimeInSeconds * 1000);
-  updateTimerDisplay();
-  displayEndTime();
-  intervalID = setInterval(decrementTimer, 1000);
+  const now = Date.now();
+  const then = now + (totalSeconds * 1000);
+  updateTimerDisplay(totalSeconds);
+  displayEndTime(then);
+  intervalID = setInterval(() => {
+    const secondsLeft = Math.round((then - Date.now()) / 1000);
+    decrementTimer(secondsLeft);
+  }, 1000);
 }
 
 buttons.forEach(button => button.addEventListener('click', startTimer));
